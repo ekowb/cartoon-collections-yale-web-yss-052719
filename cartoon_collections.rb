235c515ed3b473f596adfa94b8ac07983a45fddd
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 19:04:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |item|
+    if array.include?
+
 end
